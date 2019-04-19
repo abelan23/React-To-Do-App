fdad8dde9343ec63d1b093301a29b2585761463a
abelan23 <abelan23@gmail.com>
2019-04-19 15:23:51 -0400

    Updates

---
+++ precache-manifest.7d4e6293aa1ed111f2026542469fe691.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b0eea17bf36ae43eab5",
+    "url": "/React-To-Do-App/static/js/runtime~main.07775c96.js"
+  },
+  {
+    "revision": "4feda3ead53e53987e47",
+    "url": "/React-To-Do-App/static/js/main.92ce1908.chunk.js"
+  },
+  {
+    "revision": "af936525b6feb6ec8d22",
+    "url": "/React-To-Do-App/static/js/2.ea6acadd.chunk.js"
+  },
+  {
+    "revision": "4feda3ead53e53987e47",
+    "url": "/React-To-Do-App/static/css/main.40521dce.chunk.css"
+  },
+  {
+    "revision": "ed4d36e9309c7cf19b5dfbac46c7fbb4",
+    "url": "/React-To-Do-App/index.html"
+  }
+];
